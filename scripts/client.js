0e3ba9e118e62edc6211fa4e622cfd522ea2e99c
Brad Loudis <Bradley.Loudis@gmail.com>
2020-10-09 16:24:50 -0500

    capturing click event of Submit btn

--- scripts/client.js
+++ scripts/client.js
@@ -4,4 +4,9 @@ $(document).ready(readyUp);
 
 function readyUp() {
   console.log('JQ readyUp is working!');
+  $('.js-submitButton').on('click', addEmployee);
+}
+
+function addEmployee() {
+  console.log('submit button has been clicked!');
 }
